898fc5f62bfb3567f15050651ea53516024e792f
Akhmetov Zhanibek <zhanibek-2005@bk.ru>
2026-07-04 19:07:38 +0200

    Set up initial monorepo structure

---
+++ packages/shared/index.ts
@@ -0,0 +1,9 @@
+export type ProductCondition = "new" | "used" | "refurbished";
+
+export type PaymentMethod =
+  | "PayPal"
+  | "Credit Card"
+  | "Klarna"
+  | "Apple Pay"
+  | "Google Pay"
+  | "Bank Transfer";
